652f6557771153075ad2fd71b2668001806103b8
JIgnacioHP <ihidalgo@deustoseidor.com>
2023-10-10 11:03:26 +0200

    prueba repositorio

--- src/components/boton/boton.jsx
+++ src/components/boton/boton.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 const Boton = ({children})=>{
     return(
-        <div>hello</div>
+        <>
+            <div>hello</div>
+            <div>Prueba repositorio</div>
+        </>
     )
 
 }
