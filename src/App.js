96aaea12c4c3afef3ce79eec32c25dcb428dd883
JIgnacioHP <ihidalgo@deustoseidor.com>
2023-10-10 10:52:03 +0200

    inicio con docker y react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Boton from './components/boton/boton';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Boton/>
       </header>
     </div>
   );
